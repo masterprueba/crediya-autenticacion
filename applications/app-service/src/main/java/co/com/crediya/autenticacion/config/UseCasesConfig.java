f2a75663e4c3e1190ecae178b875bd04b97ada3f
jbreinercastro@gmail.com <jbreiner.castro@outlook.com>
2025-08-22 16:18:36 -0500

    Base clean arquitecture

---
+++ applications/app-service/src/main/java/co/com/crediya/autenticacion/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package co.com.crediya.autenticacion.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "co.com.crediya.autenticacion.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
